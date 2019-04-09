231eb23f1f229b3292a78ffce90bf639cad25357
Juan I. Castellano <jagannath>
2019-04-09 01:05:23 -0300

    fix commit

--- src/PracticalWork1/Excercise1.java
+++
@@ -1,8 +0,0 @@
-package PracticalWork1;
-
-// Write the HelloWorld program.
-public class Excercise1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
